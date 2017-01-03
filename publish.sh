341047ce49dda2510818d7326fe6d2ee22d5e7cc
Umair Kayani <ukayani@loyalty.com>
2017-01-03 16:29:51 -0500

    - added travis file which publishes to dockerhub

---
+++ publish.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [[ "$BRANCH" == "master" ]] && [[ "$TRAVIS_TAG" != "" ]] && [[ "$TRAVIS_PULL_REQUEST" == "false"]]; then
+    echo "Building docker image $DOCKER_REPO:$TRAVIS_TAG"
+    docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+    TAG="latest"
+    docker build -f Dockerfile -t $DOCKER_REPO:$COMMIT .
+    docker tag $DOCKER_REPO:$COMMIT $DOCKER_REPO:$TAG
+    docker tag $DOCKER_REPO:$COMMIT $DOCKER_REPO:$TRAVIS_TAG
+    docker push $DOCKER_REPO
+fi
+
